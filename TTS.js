39a606ca76d0300b3956f06630f1d8979b348327
Sandeep Devarapalli <Sandeep Devarapalli>
2017-04-10 11:55:33 -0700

    Text to speech(TTS)

--- TTS.js
+++ TTS.js
@@ -15,11 +15,23 @@ function TTSAppController(TTSAppService) {
     TTS.text = TTS.selectedText || '';
     TTS.talk();
   };
+
   TTS.text = '';
   TTS.talk = function () {
-    var promise = TTSAppService.talk(TTS.text);
-    console.log('end');
+    TTSAppService.talk(TTS.text);
   }
+
+  TTS.pause = function () {
+    TTSAppService.pause();
+  };
+
+  TTS.resume = function () {
+    TTSAppService.resume();
+  };
+
+  TTS.cancel = function () {
+    TTSAppService.cancel();
+  };
 }
 
 TTSAppService.$inject = ['$http'];
@@ -28,7 +40,20 @@ function TTSAppService($http) {
 
   service.talk = function (text) {
     var msg = new SpeechSynthesisUtterance(text);
+    msg.voice = speechSynthesis.getVoices()[3];
     speechSynthesis.speak(msg);
+  };
+
+  service.pause = function () {
+    speechSynthesis.pause();
+  };
+
+  service.resume = function () {
+    speechSynthesis.resume();
+  };
+
+  service.cancel = function () {
+    speechSynthesis.cancel();
   };
 }
 
